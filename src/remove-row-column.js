cfe30e240a19a312d5f5be6a84dfee83c7bbd238
Damian <damianczapiewski@gmail.com>
2024-11-06 10:40:23 +0100

    Update remove-row-column.js

--- src/remove-row-column.js
+++ src/remove-row-column.js
@@ -1,8 +1,10 @@
 import _clamp from '@utils/_clamp';
 import _clone from '@utils/_clone';
+import order from './order';
 
 export function removeRow(M,m=M.length) {
-	m = _clamp(m,1,M.length);
+	const [rows] = order(M);
+	m = _clamp(m,1,rows);
 	return _clone(
 		M,
 		(_,i) => i !== (m-1)
@@ -10,7 +12,8 @@ export function removeRow(M,m=M.length) {
 }
 
 export function removeColumn(M,n=M[0].length) {
-	n = _clamp(n,1,M[0].length);
+	const [,cols] = order(M);
+	n = _clamp(n,1,cols);
 	return _clone(
 		M,
 		() => true,
