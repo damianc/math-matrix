4b57a2f5667effae569eade9b9c5924b3efa79d1
Damian <damianczapiewski@gmail.com>
2024-11-06 05:19:15 +0100

    Update _operation.js

--- src/utils/_operation.js
+++ src/utils/_operation.js
@@ -2,6 +2,7 @@ import assert from '@assert';
 
 export default function _operation(A,B,f) {
 	assert.compatibleMatrices(A,B);
+	
 	return A.map((row,i) => {
 		return row.map((item,j) => {
 			return f(item, B[i][j]);
