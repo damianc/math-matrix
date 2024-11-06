c6eb2b1602130450cc610505c248e4582e531862
Damian <damianczapiewski@gmail.com>
2024-11-06 05:09:51 +0100

    Update index.js

--- src/assert/index.js
+++ src/assert/index.js
@@ -1,11 +1,13 @@
 import compatibleMatrices from '@assert/compatible-matrices';
 import multiplicableMatrices from '@assert/multiplicable-matrices';
 import isSquareMatrix from '@assert/is-square-matrix';
+import isMinorable from '@assert/is-minorable';
 
 const assert = {
   compatibleMatrices,
   multiplicableMatrices,
-  isSquareMatrix
+  isSquareMatrix,
+  isMinorable
 };
 
 export default assert;
