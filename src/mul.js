24c3d8074a9df7ef3921f5abeec69f8cd0bf566b
Damian <damianczapiewski@gmail.com>
2024-11-06 04:46:54 +0100

    Create mul.js

---
+++ src/mul.js
@@ -0,0 +1,24 @@
+import { assert } from '@assert';
+import order from './order';
+import { initNullMatrix } from './init-matrix';
+
+export default function multiply(A,B) {
+  assert.multiplicableMatrices(A,B);
+	
+  const [rowsA,colsA] = order(A);
+  const [,colsB] = order(B);
+  const C = initNullMatrix(rowsA,colsB);
+ 
+  for (let i=0; i<rowsA; i++) {
+    for (let j=0; j<colsB; j++) {
+      let sum = 0;
+      for (let k=0; k<colsA; k++) {
+        sum += A[i][k] * B[k][j];
+      }
+      C[i][j] = sum;
+    }
+  }
+  
+  return C;
+}
+
