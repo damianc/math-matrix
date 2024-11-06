61b33e4d3c5735b77089998ee8d50e2f173e0238
Damian <damianczapiewski@gmail.com>
2024-11-06 05:27:05 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import add from './add';
+import sub from './sub';
+import mul from './mul';
+import mulByScalar from './mul-by-scalar';
+import order from './order';
+import trace from './trace';
+import det from './det';
+import transpose from './transpose';
+import { minor, minorize } from './minor';
+import {
+  initNullMatrix, initScalarMatrix, initIdentityMatrix
+} from './init-matrix';
+
+(function () {
+  
+  Math.matrix = {
+    add,
+    sub,
+    mul,
+    mulByScalar,
+    order,
+    trace,
+    det,
+    transpose,
+    minor,
+    minorize,
+    initNullMatrix,
+    initScalarMatrix,
+    initIdentityMatrix
+  };
+  
+})()
