3ab297fd000b250cdc7987f71a8873e3d2fee897
Damian <damianczapiewski@gmail.com>
2024-11-06 11:40:51 +0100

    Update is-matrix.js

--- src/is-matrix.js
+++ src/is-matrix.js
@@ -4,3 +4,21 @@ export function isSquare(M) {
   const [rows,cols] = order(M);
   return rows === cols;
 }
+
+function equal(A,B) {
+	const [rowsA,colsA] = order(A);
+	const [rowsB,colsB] = order(B);
+	if (
+		(rowsA!==rowsB) || (colsA!==colsB)
+	) {
+		return false;
+	}
+	return A.every((row,i) => {
+		if (row.length !== B[i].length) {
+			return false;
+		}
+		return row.every((col,j) => {
+			return col === B[i][j];
+		});
+	});
+}
